41b5c11f6327f1c1aa5a8fee40c98eb3dcc987d8
madeindreams <madeindreams@protonmail.com>
2021-10-27 07:05:36 -0400

    added test unit

--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
 import { assert, expect } from 'chai'
 import RPC from '../modules/idfi-rpc.js'
 
-describe('ERC20', () => {
-    it('Responds with correct symbol', () => {
+describe('Chain Difficulty', () => {
+    it('Responds with correct difficulty', () => {
         const rpc = new RPC(1, "0.0.0.0", 9998, 'idecentralize');
-        console.log(rpc)
         expect(rpc.chain.difficulty).to.equal(2)
                        
     })
